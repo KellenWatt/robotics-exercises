b5dc58f398d0b11e016f70ae91928bfcea58b3c8
Kellen Watt <kbw6d9@mst.edu>
2020-08-19 20:52:39 -0500

    Add hard exercise for variables

---
+++ c++/01-Variables/hard/hard.cpp
@@ -0,0 +1,7 @@
+/*
+ * Create a full program, including main, that declares and initializes it,
+ * then assigns the value of that variable to another variable. The variables can 
+ * have whatever value you want, be of whatever type you want, and be called 
+ * whatever you want. You have full discretion in what this program looks like. 
+ * Just demonstrate a familiarity with the concepts involved.
+ */
