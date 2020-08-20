3d29d2ca685bf4df8be0741c05e38bad97ecdd00
Kellen Watt <kbw6d9@mst.edu>
2020-08-19 22:22:35 -0500

    Add hard I/O problem

---
+++ c++/02-IO/hard/problem.cpp
@@ -0,0 +1,10 @@
+/*
+ * Write a full program that acts as a greeting program.
+ *
+ * Whenever you run the program, it should prompt the user to enter a name by 
+ * printing a message to the screen. Then you need to take in a name and print 
+ * the string "Hi, <input name>! Welcome to the party!".
+ *
+ * For example, if the user types "Alex" as their name, then your program should
+ * print "Hi, Alex! Welcome to the party!".
+ */
