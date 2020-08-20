6c876a3580b309c6b5574cd64299ac693e8f2bd0
Kellen Watt <kbw6d9@mst.edu>
2020-08-19 22:22:06 -0500

    Add easy IO problems

---
+++ c++/02-IO/easy/problem1.md
@@ -0,0 +1,7 @@
+Answer the following questions.
+
+1. What's the first thing you need to do to use `std::cout` and `std::cin`
+
+2. What does `cout` do?
+
+3. What does `cin` do?
---
+++ c++/02-IO/easy/problem2.cpp
@@ -0,0 +1,12 @@
+/*
+ * Fill in the blanks in the following program so that it prints "Hi, my name is <your name>!".
+ */
+
+#include <________>
+#include <string>
+
+
+int main() {
+    std::string myName = ______;
+    _________ << "Hi, my name is " << myName << _________;
+}
