40060780605966036fceb5e030d8acace660cf48
Kellen Watt <kbw6d9@mst.edu>
2020-08-20 17:09:24 -0500

    Add level 2 coding

---
+++ c++/03-Operators/level2/problem2.cpp
@@ -0,0 +1,49 @@
+/*
+ * Below are several groups of statements consisting of a variable declaration 
+ * followed by a `cout` statement. Add a line with an assignment operator 
+ * between the variable declaration and the `cout` statement to print out 
+ * the correct value.
+ *
+ * Do not modify the varaible declaration or `cout` statement.
+ */
+#include <iostrea>
+
+using std::cout;
+using std::endl;
+
+
+int main() {
+    int n = 5;
+    // 11
+    cout << n << endl;
+
+    double d = 5.7;
+    // 17.1 (mult)
+    cout << d << endl;
+
+    int i = 10;
+    // 11
+    cout << i << endl;
+
+    int k = 4;
+    // 3
+    cout << k << endl;
+
+    float f = 15.8;
+    // 13.6 (sub)
+    cout << f << endl;
+
+    double x = 54.6;
+    // 9.1 (div)
+    cout << x << endl;
+
+    int y = 21;
+    // 4 (div)
+    cout << y << endl;
+
+    int z = 43;
+    // 3 (mod)
+    cout << z << endl;
+}
+
+
