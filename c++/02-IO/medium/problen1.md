5456c73223feb99c6ee1c12c75ab4345eb32afcf
Kellen Watt <kbw6d9@mst.edu>
2020-08-19 22:22:22 -0500

    Add medium I/O problems

---
+++ c++/02-IO/medium/problen1.md
@@ -0,0 +1,5 @@
+Answer the following questions.
+
+1. Does `endl` work with `cin`?
+
+2. How do you let `cin` know that you're done typing?
---
+++ c++/02-IO/medium/problen2.cpp
@@ -0,0 +1,14 @@
+/*
+ * Write the necessary code to create an integer variable and read input into it. 
+ * The value can be whatever you want, it just can't be a fixed value at compile-time.
+ *
+ * Feel free to output the value you get from input, though this isn't a requirement.
+ */
+
+#include <iostream>
+
+int main() {
+    // Add your code here.
+
+
+}
