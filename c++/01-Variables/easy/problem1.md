0af995e9ae0c5cc2a8d5be77731ea759990e96ba
Kellen Watt <kbw6d9@mst.edu>
2020-08-19 19:42:45 -0500

    Add easy problems for variables

---
+++ c++/01-Variables/easy/problem1.md
@@ -0,0 +1,11 @@
+Answer the following questions about types:
+
+1. What is a data type?
+
+2. What type is the value `4876`?
+
+3. What is the approximate maximum value of a `double`?
+
+4. What are the possible values in for a `bool`?
+
+5. What is a string, and how do you define a literal string?
---
+++ c++/01-Variables/easy/problem2.cpp
@@ -0,0 +1,23 @@
+/*
+ * This is a multiline comment. This is about the only place you should use one.
+ *
+ * Create 6 variables in main and give them initial values, one each, of the 
+ * following types:
+ *   - int
+ *   - float
+ *   - double
+ *   - bool
+ *   - string
+ *   - char
+ *
+ * Make sure your code compiles and runs before you submit. It doesn't need to do 
+ * anything, just make sure it's syntactically correct. You can name the 
+ * variables whatever you want, as long as you follow a consistant style.
+ */
+
+int main() {
+    // Write your variables here
+    
+
+}
+
