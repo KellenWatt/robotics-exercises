cd6553d8876a07be9352b5b9d332d71f6fbea652
Kellen Watt <kbw6d9@mst.edu>
2020-08-21 14:04:56 -0500

    Add level 3 exercise on 04-Branching

---
+++ c++/04-Branching/level3/problem.cpp
@@ -0,0 +1,7 @@
+/*
+ * Write a program that takes a number from input, then prints whether it's 
+ * even. What you print is up to you.
+ *
+ * You can check if a number is even by checking if it is divisible by 2 or 
+ * if the remainder when dividing by 2 is 0.
+ */
