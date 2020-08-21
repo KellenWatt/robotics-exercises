e7e892be3a9b4cf25ac845cac9fb9380f9eb48d8
Kellen Watt <kbw6d9@mst.edu>
2020-08-21 13:52:46 -0500

    Add level 2 switch problems for 04-Branching

---
+++ c++/04-Branching/level2/problem1.md
@@ -0,0 +1,5 @@
+Answer the following questions.
+
+1. How many cases can you have in a `switch` statement? How many `default` cases?
+
+2. What is the concept of scope? Provide a summary or an example of how scope works.
---
+++ c++/04-Branching/level2/problem2.cpp
@@ -0,0 +1,20 @@
+/*
+ * Write a switch statement that, given a number from 1 to 7, prints the day 
+ * of the week associated with that number, starting with Sunday, and ending
+ * with Saturday. Make sure you can handle any possible day of the week.
+ *
+ * For example, if given 2, the case should contain `cout << "Tuesday";`.
+ */
+
+#include <iostream>
+
+int main() {
+    // Switch this value to test the various possiblities.
+    int dayOfWeek = 5; 
+    // Add your switch statement here
+
+
+
+    // Don't write anything after this.
+    std::cout << std::endl;
+}
